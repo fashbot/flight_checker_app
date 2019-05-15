27f962a054c8c68fc7f278e8e5683f4183092390
Stephanie <stephanie.o.fashagba@gmail.com>
2019-05-15 15:49:43 +0100

    Add airport mock data class

---
+++ app/src/main/java/yass/stephanie/com/flight_checker/ui/findfragment/AirportData.kt
@@ -0,0 +1,19 @@
+package yass.stephanie.com.flight_checker.ui.findfragment
+
+data class AirportData(
+    val PEK: String = "Beijing Capital International Airport (PEK)",
+    val DXB: String = "Dubai International Airport (DXB)",
+    val LAX: String = "Los Angeles International Airport (LAX)",
+    val HND: String = "Tokyo Haneda Airport (HND)",
+    val ORD: String = "O’Hare International Airport, Chicago (ORD)",
+    val LHR: String = "London Heathrow (LHR)",
+    val HKA: String = "Hong Kong International Airport (HKA)",
+    val PVG: String = "Shanghai Pudong International Airport (PVG)",
+    val CGD: String = "Paris Charles de Gaulle Airport (CDG)",
+    val AMS: String = "Amsterdam Airport Schiphol (AMS)",
+    val DEL: String = "Indira Gandhi International Airport (DEL)",
+    val FRA: String = "Frankfurt Airport (FRA)",
+    val ICN: String = "Seoul Incheon International Airport (ICN) ",
+    val IST: String = "Istanbul Atatürk Airport (IST)",
+    val DEN: String = "Denver International Airport (DEN)"
+)
--- app/src/main/java/yass/stephanie/com/flight_checker/ui/findfragment/RoundTripViewModel.kt
+++ app/src/main/java/yass/stephanie/com/flight_checker/ui/findfragment/RoundTripViewModel.kt
@@ -3,15 +3,40 @@ package yass.stephanie.com.flight_checker.ui.findfragment
 import android.arch.lifecycle.ViewModel;
 
 class RoundTripViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private lateinit var airports: MutableList<String>
+    private var data = AirportData()
+
+    init{
+        populateAirport()
+    }
 
 
+    private fun getAirportList(): MutableList<String>{
+        return airports
+    }
 
 
-
-
-
-
+    private fun populateAirport() {
+        val dataList = listOf<String>(
+            data.AMS,
+            data.CGD,
+            data.DEL,
+            data.DEN,
+            data.DXB,
+            data.FRA,
+            data.HKA,
+            data.HND,
+            data.ICN,
+            data.IST,
+            data.LAX,
+            data.LHR,
+            data.ORD,
+            data.PEK,
+            data.PVG
+        )
+        airports.addAll(dataList)
+    }
 
 
 }
